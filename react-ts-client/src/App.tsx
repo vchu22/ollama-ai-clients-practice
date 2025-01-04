3f1f8a1157df6010f6ab13190768a0e8a93c263b
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-01-04 13:39:47 -0500

    use Axios library to get a list of models on the Ollama server

--- react-ts-client/src/App.tsx
+++ react-ts-client/src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const portocol = "http", host = "localhost", port = 10000;
+const rootURL = `${portocol}://${host}:${port}`;
+
 function App() {
+  const [JSONResponse, setJSONResponse] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${rootURL}/api/tags`)
+      .then(response => {
+        setJSONResponse(response.data.models);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {JSONResponse.map((data : {
+          modified_at: string,
+          name: string
+        }) => (
+          <li key={data.modified_at}>{data.name}</li>
+        ))}
+    </ul>
   );
 }
 
